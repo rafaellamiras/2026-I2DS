b94280578c0a34fbabaf024023a46fb8e5d321bb
rafaellamiras <rafaellamiras@gmail.com>
2026-08-04 11:25:14 -0300

    Revisão de Fundamentos de C# - P1

--- Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
+++ Aula-02/POO/obj/Debug/net10.0/POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378e2dcb333d52a64abe118198007b15d768bb2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b706b5ed3fba7db66a1cab4d7910c0c16f63775")]
 [assembly: System.Reflection.AssemblyProductAttribute("POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
