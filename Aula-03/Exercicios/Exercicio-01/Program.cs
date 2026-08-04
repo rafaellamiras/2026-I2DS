f568d53a1e237f8b0ff05f7f184472f859289641
rafaellamiras <rafaellamiras@gmail.com>
2026-08-04 17:04:29 -0300

    exercicios

--- Aula-03/Exercicios/Exercicio-01/Program.cs
+++ Aula-03/Exercicios/Exercicio-01/Program.cs
@@ -2,8 +2,12 @@
 {
     private static void Main(string[] args)
     {
-        
+        int celsius;
+        Console.Write("Digite a temepratura em Graus Celsius: ");
+        celsius = int.Parse(Console.ReadLine());
 
+        double fahrenheit = (celsius * 9 / 5) + 32;
 
+        Console.WriteLine("A temepratura em fahrenheit é: " + fahrenheit);
     }
 }
---
+++ Aula-03/Exercicios/Exercicio-02/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num1;
+        int num2;
+        Console.Write("Digite o Primeiro Número: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o Segundo Número: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        string operacao;
+        
+        Console.Write("Selecione uma Operação: ");
+        operacao = Console.ReadLine();
+
+        switch (operacao)
+        {
+            case "1": 
+                Console.WriteLine(num1 + num2);
+                break;
+            case "2": 
+                Console.WriteLine(num1 - num2);
+                break;
+            case "3": 
+                Console.WriteLine(num1 * num2);
+                break;
+            case "4": 
+                Console.WriteLine(num1 / num2);
+                break;
+            default:
+                Console.WriteLine("Operador informado não é válido.");
+                break;
+        }
+    }
+}
