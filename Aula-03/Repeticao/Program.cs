86af93e34d0bbd42f41417f8ab7112f79486ce53
rafaellamiras <rafaellamiras@gmail.com>
2026-08-04 14:58:59 -0300

    Revisão de Vetores

--- Aula-03/Repeticao/Program.cs
+++ Aula-03/Repeticao/Program.cs
@@ -56,6 +56,8 @@ int num;
         {
             Console.Write("Digite um Numero Positivo: ");
             num = int.Parse(Console.ReadLine());
-        }while(num <=0);
+        }while(num <= 0);
+
+
     }
 }
---
+++ Aula-03/Vetores/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /* Vetores e Matrizes 
+        Vetor => é uma estrtura de dados utilizada para armazenar varios valores de um mesmo tipo dentro de uma única variavel
+
+        Sintaxe: tipo[] nome_vetor =new tipo[tamanho];
+        */
+        
+
+        /* Iniciar um vetor -> lista de frutas */
+
+        string[] listaFrutas = {"Maça", "Manga", "Morango", "Melancia", "Melão"};
+
+        Console.WriteLine("Acessar o terceiro elemento da lista: ");
+        Console.WriteLine(listaFrutas [2]);
+        
+        Console.WriteLine("Alterar o segundo elemento da lista: ");
+        listaFrutas[1] = "Banana";
+
+        for(int indice = 0; indice < listaFrutas.Count(); indice++ )
+        {
+            Console.WriteLine(listaFrutas[indice]);
+        }
+        
+        /* ****************************************************************** */
+        foreach(string fruta in listaFrutas)
+        {
+            Console.WriteLine(fruta);
+        }
+        }
+}
