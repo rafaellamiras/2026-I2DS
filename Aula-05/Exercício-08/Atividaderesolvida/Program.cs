b4af121948e57f819b76357b7da736ef50325cf8
rafaellamiras <rafaellamiras@gmail.com>
2026-08-11 16:56:00 -0300

    Resolução exercicio 08 - 2

--- Aula-05/Exercício-08/Atividaderesolvida/Program.cs
+++ Aula-05/Exercício-08/Atividaderesolvida/Program.cs
@@ -1,7 +1,9 @@
-﻿public class Personagem
+﻿internal class Program
+{
+    public class Personagem
     {
         public string Nome {get; private set;}
-        public int Nivel {get; private set;}
+        public int Nivel {get; protected set;}
         public int Experiencia {get; private set;}
         public int Forca {get; protected set;}
         public int Agilidade {get; protected set;}
@@ -76,7 +78,7 @@
         protected virtual void Evoluir()
         {
             Nivel++; // Nivel = Nivel + 1
-            Forca ++; // Forca = Forca + 2
+            Forca += 2; // Forca = Forca + 2
             Agilidade += 2;
             Inteligencia +=2;
             Vida += 10;
@@ -99,3 +101,50 @@
             return Vida > 0;
         }
     }
+    // ======================================================
+    // MAGO
+    // ======================================================
+    public class Mago : Personagem
+    {
+        public Mago(string nome): base(nome, 5, 7, 25, 90){ }
+
+        public override void Atacar(Personagem alvo)
+        {
+            int dano = Inteligencia;
+            Console.WriteLine(
+                $"{Nome} lançou uma magia contra {alvo.Nome}");
+            alvo.Defender(dano);
+            GanharExperiencia(20);
+        }
+
+        protected override void Evoluir()
+        {
+            Nivel++;
+            Forca++; // Forca = Forca + 2
+            Agilidade += 2;
+            Inteligencia +=5;
+            Vida += 15;
+
+            Console.WriteLine();
+            Console.WriteLine("========================================");
+            Console.WriteLine("              LEVEL UP!");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+            Console.WriteLine($"{Nome} evoluiu para o nível {Nivel}");
+            Console.WriteLine($"Força: {Forca}");
+            Console.WriteLine($"Agilidade: {Agilidade}");
+            Console.WriteLine($"Inteligência: {Inteligencia}");
+            Console.WriteLine($"Vida: {Vida}");
+            Console.WriteLine("========================================");
+            Console.WriteLine();
+
+
+        }
+    }
+
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
